eaf55c0f4f2bb37416c372e977009a8cc6a3d6f8
natalia646 <nataxash46@gmail.com>
2022-08-02 14:00:30 +0000

    task 1 2

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// const radius = Number(prompt('Введіть радіус кола'));
+// const circle = Math.PI * radius * radius;
+// alert (circle);
+
+// const age = Number(prompt('Скільки Вам років?'));
+// const pension = 65 - age;
+// const year = 2022 + pension;
+// alert(pension);
+// alert(year); 
+
+
+const number = prompt('Введіть трицифрове число');
+
