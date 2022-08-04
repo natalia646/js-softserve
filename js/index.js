529252a4dba0f96e6abc75944fe79d7c20002857
natalia646 <nataxash46@gmail.com>
2022-08-04 10:31:57 +0000

    taks 3

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
+// taks 1
 const radius = Number(prompt('Введіть радіус кола'));
 const circle = Math.PI * radius **2;
 alert (circle);
 
+// taks 2
 const age = Number(prompt('Скільки Вам років?'));
 const pension = 65 - age;
 const year = 2022 + pension;
 alert(pension);
 alert(year); 
 
-
+// taks 3
 const number = prompt('Введіть трицифрове число');
+const conversely = number.split("").reverse().join("");
+alert(conversely);
 
