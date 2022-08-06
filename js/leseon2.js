70efa9cdaa4daf9ca41ed5dd2763df871f19053a
natalia646 <nataxash46@gmail.com>
2022-08-06 14:45:57 +0000

    task 1

---
+++ js/leseon2.js
@@ -0,0 +1,18 @@
+// // task 1
+// const length = Number(prompt('Введіть довжину приміщення'));
+// const width = Number(prompt('Введіть ширину приміщення'));
+// const height = Number(prompt('Введіть висоту приміщення'));
+
+// const volumeRoom = length * width * height;
+// const paint = volumeRoom / 16;
+
+// if (length >= 1000 || width >= 1000 || height >= 1000) {
+//     alert('Цe дуууже велике приміщення');
+// } else {
+//     alert(`Вам потрібно ${paint} літрів фарби`);
+// }
+
+
+
+
+
