3b2fc180b6bf5f5f4966e3402a9f05597f4eb07f
natalia646 <nataxash46@gmail.com>
2022-08-08 15:17:10 +0000

    task 3

--- js/leseon2.js
+++ js/leseon2.js
@@ -6,22 +6,36 @@ const volumeRoom = l * w * h;
 const paint = volumeRoom / 16;
 
 if (l >= 1000 || w >= 1000 || h >= 1000) {
-    alert('Цe дуууже велике приміщення');
+  alert('Цe дуууже велике приміщення');
 } else {
-    alert(`Вам потрібно ${paint} літрів фарби`);
+  alert(`Вам потрібно ${paint} літрів фарби`);
 }
 
 
 
 //task 2
-const celsius  = prompt('Enter ℃');
+const celsius = prompt('Enter ℃');
 const fahrenheit = celsius * 1.8 + 32;
 
-if (celsius == Number(celsius)){
+if (celsius == Number(celsius)) {
   alert(`Temperature ${fahrenheit} ℉`);
-}else{
+} else {
   alert("I don't understand you")
 }
 
 
+//task 3
+const ticket = prompt('Enter');
+const first = +ticket[0] + +ticket[1] + +ticket[2];
+const last = +ticket[3] + +ticket[4] + +ticket[5];
+
+if (ticket.length === 6) {
+  if (first === last) {
+    alert('Woow! You have a lucky ticket!');
+  } else {
+    alert("Not today")
+  }
+} else {
+  alert("Must be 6 digits")
+}
 
