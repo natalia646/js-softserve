612a956fb21d3dfe10d0ec1ab935ef3d23c3b085
natalia646 <nataxash46@gmail.com>
2022-08-08 09:01:14 +0000

    task 2

--- js/leseon2.js
+++ js/leseon2.js
@@ -1,18 +1,26 @@
-// // task 1
-// const length = Number(prompt('Введіть довжину приміщення'));
-// const width = Number(prompt('Введіть ширину приміщення'));
-// const height = Number(prompt('Введіть висоту приміщення'));
+// task 1
+const l = Number(prompt('Введіть довжину приміщення'));
+const w = Number(prompt('Введіть ширину приміщення'));
+const h = Number(prompt('Введіть висоту приміщення'));
 
-// const volumeRoom = length * width * height;
-// const paint = volumeRoom / 16;
+const volumeRoom = l * w * h;
+const paint = volumeRoom / 16;
 
-// if (length >= 1000 || width >= 1000 || height >= 1000) {
-//     alert('Цe дуууже велике приміщення');
-// } else {
-//     alert(`Вам потрібно ${paint} літрів фарби`);
-// }
+if (l >= 1000 || w >= 1000 || h >= 1000) {
+    alert('Цe дуууже велике приміщення');
+} else {
+    alert(`Вам потрібно ${paint} літрів фарби`);
+}
 
+//task 2
+const celsius  = prompt('Enter ℃');
+const fahrenheit = celsius * 1.8 + 32;
 
+if (celsius == Number(celsius)){
+  alert(`Temperature ${fahrenheit} ℉`);
+}else{
+  alert("I don't understand you")
+}
 
 
 
