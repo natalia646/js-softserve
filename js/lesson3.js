491b2fcab9f350ff5702eec9efca775deae55e35
natalia646 <nataxash46@gmail.com>
2022-08-22 13:50:53 +0000

    codeWors

--- js/lesson3.js
+++ js/lesson3.js
@@ -15,20 +15,20 @@
 // }
 //     alert(arrDNA.join(''));
 
-//task 2
-const sand = prompt("Enter kg sand").split(",");
-const box = prompt("Enter volume boxes").split(",");
+// //task 2
+// const sand = prompt("Enter kg sand").split(",");
+// const box = prompt("Enter volume boxes").split(",");
 
-let sortSand = sand.sort((a, b) => a - b);
-let sortBox = box.sort((a, b) => a - b)
+// let sortSand = sand.sort((a, b) => a - b);
+// let sortBox = box.sort((a, b) => a - b)
 
-function theMostMoney(sortSand, sortBox) {
-    for (let j = 0; j < sortSand.length; j++) {
-        for (let i = 0; i < sortBox.length; i++) {
-            return sortSand[0] * sortBox[0] + sortSand[1] * sortBox[1] + sortSand[2] * sortBox[2];
-        }
-    }
-}
-let alertMoney = theMostMoney(sortSand, sortBox);
-alert(`You have ${alertMoney}$`)
+// function theMostMoney(sortSand, sortBox) {
+//     for (let j = 0; j < sortSand.length; j++) {
+//         for (let i = 0; i < sortBox.length; i++) {
+//             return sortSand[0] * sortBox[0] + sortSand[1] * sortBox[1] + sortSand[2] * sortBox[2];
+//         }
+//     }
+// }
+// let alertMoney = theMostMoney(sortSand, sortBox);
+// alert(`You have ${alertMoney}$`)
 
--- js/lesson5.js
+++ js/lesson5.js
@@ -1,5 +1,5 @@
-const arr = ['school', 'table', 2, 4, 5.4, 'museum', 'cat', 'dog'];
-console.log(arr)
+// const arr = ['school', 'table', 2, 4, 5.4, 'museum', 'cat', 'dog'];
+// console.log(arr)
 ////////splice//////
 // const sp1 = arr.splice(2, 3); починаючи з 2 індекса видаляє 3 улументи
 // const sp2 = arr.splice(2, 3, 'chair', 'nose'); починаючи з 2 індекса дидаляє 3 елементи, а натомість вставляє два нові: 'chair', 'nose' 
@@ -9,3 +9,34 @@ console.log(arr)
 // const sl2 = arr.slice(5)// копіює з 5 індукса до кінця
 // const sl3 = arr.slice(1,-2); // коп з 1 індуксу до 2 зі заду
 // const sl4 = arr.slice(-3)// коп остані 3
+
+
+// const ar = [1, 2, 3, 6, 7, 8, 9, 12, 15];
+// console.log(ar);
+// const ar1 = ar.slice();
+
+// function removeEven(arr) {
+//     for (let i = arr.length; i >= 0; i--)
+//         if (arr[i] % 2) arr.splice(i, 1)
+//     return arr
+// }
+// console.log(removeEven(ar1))
+
+// function threeInOne(arr) {
+//     let arrCopy = arr.slice();
+//     let arrNew = [];
+//     for (let i = arrCopy.length; i >= 0; i--) {
+//         for (let j = i / 3; j >= 0; j--) {
+//             let arrSumm = arrCopy[i] + arrCopy[i + 1] + arrCopy[i + 2];
+//          arrNew.push(arrSumm)
+//         }
+//     }
+// }
+
+function threeInOne(arr) {
+    let arrCopy = arr.slice();
+    let arrNew = [];
+    for (let i = 0; i< arrCopy.length; i+=3){
+        arrNew.push(arr[i] + arr[i+1] + arr[i+2]);
+    }
+}
